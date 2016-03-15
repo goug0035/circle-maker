b6603a5e49d7c69e3838cc978398ae6340b036af
goug0035 <marygough@Marys-MacBook-Pro.local>
2016-03-15 16:04:49 -0400

    setup

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var $ball = $('.ball');
+
+var $html = $('html');
+
+$html.on('keydown', function () {
+  console.log(e.keyCode);
+  var div = document.createElement('div');
+  var rand = Math.floor(Math.random() * (max - min)) + min;
+});
